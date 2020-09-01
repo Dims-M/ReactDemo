ac4aeba1818208c3387ae170a07140b5580670cf
Dims-M <dimsxm@yandex.ru>
2020-09-01 17:45:09 +0300

    экперименты

---
+++ ReactDemo/wwwroot/js/tutorial.js
@@ -0,0 +1,9 @@
+﻿class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">Здравствуй, мир! Я-окно комментариев.</div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox />, document.getElementById('content'));
